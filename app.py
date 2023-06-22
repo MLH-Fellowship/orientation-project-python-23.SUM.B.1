8774b44c2f1369ffc7eec3c5d098e5bb9b9e511a
baimamboukar <baimamboukar@gmail.com>
2023-06-22 19:19:03 +0100

    FEAT: post requests validation

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ Flask Application
 '''
 from flask import Flask, jsonify, request
 from models import Experience, Education, Skill
+from utils import validate_date_string, validate_grade, validate_proficiency, validate_request
 
+
+validate_date_string
 app = Flask(__name__)
-
+# Validation of required fields
 data = {
     "experience": [
         Experience("Software Developer",
@@ -48,7 +51,19 @@ def experience():
         return jsonify()
 
     if request.method == 'POST':
-        return jsonify({})
+         # Request validation Start
+        body = request.json
+        required_fields = ['title', 'company', 'start_date', 'description', 'logo']
+        if body.get('start_date') and not validate_date_string(body.get('start_date')):
+            return jsonify({"Error": "Invalid start date. The date format should be `June 2023`"}), 400
+        if(body.get('end_date') and not validate_date_string(body.get('end_date'))):
+            return jsonify({"Error": "Invalid end date. The date format should be like `June 2023`"}), 400
+        if not validate_request(body, required_fields):
+            return jsonify({"Error": "Invalid request payload. Some required attributes are missing"}), 400
+        
+        # Request validation End
+        
+        return jsonify({}), 201
 
     return jsonify({})
 
@@ -61,7 +76,22 @@ def education():
         return jsonify({})
 
     if request.method == 'POST':
-        return jsonify({})
+        
+        # Request validation Start
+        body = request.json
+        required_fields = ['course', 'school', 'start_date', 'grade', 'logo']
+        if(body.get('grade') and not validate_grade(body.get('grade'))):
+            return jsonify({"Error": "Invalid grade. The grade should be one of `A+, A, A-, B+, B, B-, C+, C, C-, D+, D, D-, F`"}), 400
+        if body.get('start_date') and not validate_date_string(body.get('start_date')):
+            return jsonify({"Error": "Invalid start date. The date format should be `June 2023`"}), 400
+        if(body.get('end_date') and not validate_date_string(body.get('end_date'))):
+            return jsonify({"Error": "Invalid end date. The date format should be like `June 2023`"}), 400
+        if not validate_request(body, required_fields):
+            return jsonify({"error": "Invalid request payload. Some required attributes are missing"}), 400
+        
+        # Request validation End
+        
+        return jsonify({}), 201
 
     return jsonify({})
 
@@ -75,6 +105,11 @@ def skill():
         return jsonify({})
 
     if request.method == 'POST':
-        return jsonify({})
+        body = request.json
+        required_fields = ['name', 'proficiency', 'logo']
+        if(body.get('proficiency') and not validate_proficiency(body.get('proficiency'))):
+            return jsonify({"Error": "Invalid proficiency format. The proficiency format should look like `72%` "}), 400
+        
+        return jsonify({}), 201
 
     return jsonify({})
---
+++ utils.py
@@ -0,0 +1,61 @@
+from datetime import datetime
+
+
+'''
+validate_date_string
+🍀 `vaidate_date_string` takes a date string as input.
+✅ It attempts to parse the date string using the strptime method from datetime module
+✅ The [%B %Y] directives are used to match date's full month name and year respectively.
+✅ It returns True if the date string format is valid, False otherwise.
+
+'''
+def validate_date_string(date_string):
+    if date_string.lower() in ["present", "now", "current", "till date", "till now"]:
+        return True
+    try:
+        datetime.strptime(date_string, "%B %Y")
+        return True
+    except ValueError:
+        return False
+'''
+validate_request
+🍀 `validate_request` takes a payload and a list of required fields as input.
+✅ It iterates over the list of required fields and checks if each field is present in the payload.
+✅ It returns True if all the required fields are present in the payload, False otherwise.
+'''
+def validate_request(payload, required_fields) -> bool:
+    for field in required_fields:
+        if field not in payload:
+            return False
+    return True
+
+
+'''
+validate_proifciency
+🍀 `validate_proficiency` takes a proficiency string as input.
+✅ It checks if the proficiency string ends with the `%` character.
+✅ It attempts to convert the proficiency string to an integer.
+✅ It returns True if the proficiency string is a valid integer between 0 and 100, False otherwise.
+'''
+def validate_proficiency(proficiency)-> bool:
+    if proficiency.endswith('%'):
+        proficiency_value = proficiency[:-1]
+        try:
+            proficiency_value = int(proficiency_value)
+            if proficiency_value >= 0 and proficiency_value <= 100:
+                return True
+        except ValueError:
+            pass
+    return False
+
+'''
+validate_grade
+🍀 `validate_grade` takes a grade string as input.
+✅ It checks if the grade string is present in the list of valid grades.
+✅ It returns True if the grade string is valid, False otherwise.
+'''
+def validate_grade(grade):
+    valid_grades = ["A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D", "D-", "F"]
+    if grade.upper() in valid_grades:
+        return True
+    return False
