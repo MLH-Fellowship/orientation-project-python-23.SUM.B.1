8ef6673243f129177f3b55418ddc8fb3be084386
suelenk <suelenmatosr@gmail.com>
2023-06-27 17:21:44 -0400

    Developed json files to hold initial data to seed database with. Added logic to seed database with data from data folder

--- seed_database.py
+++ seed_database.py
@@ -1,12 +1,75 @@
+"""This file Loads data from json file and seed the tables"""
+
 import database_model
 import os
 import json
 import crud
 import database_model
-# from database_model import app_temporary
+
 # import app
 
 os.system("dropdb resume")
 os.system("createdb resume")
 database_model.connect_to_db(database_model.app_temporary, "resume")
 database_model.db.create_all()   #create database structure
+
+
+with open('data/user.json') as file:
+    user_file= json.loads(file.read())
+
+db_users = []
+for user in user_file:
+    name = user['name']
+    phone = user['phone']
+    email = user['email']
+    resume_order = user['resume_order']
+
+    save_user_to_db = crud.create_user(name, phone, email, resume_order)
+    db_users.append(save_user_to_db)
+database_model.db.session.add_all(db_users)    
+
+
+with open('data/education.json') as education_file:
+    educations = json.loads(education_file.read())
+
+education_list = []
+for education in educations:
+       course = education['course']
+       school= education['school']
+       start_date = education['start_date']
+       end_date = education['end_date']
+       grade = education['grade']
+       logo = education['logo']
+
+       save_education_to_db = crud.create_education(course, school, start_date, end_date, grade, logo)
+       education_list.append(save_education_to_db)
+database_model.db.session.add_all(education_list)   
+
+
+with open('data/experience.json') as experience_file:
+    experiences = json.loads(experience_file.read())
+
+experience_list = []
+for experience in experiences:
+    title = experience['title']
+    company = experience['company']
+    start_date = experience['start_date']
+    end_date = experience['end_date']
+    description = experience['description']
+    logo = experience['logo']
+
+    save_experience_to_db = crud.create_experience(title, company, start_date, end_date, description, logo)
+    experience_list.append(save_experience_to_db)
+database_model.db.session.add_all(experience_list)    
+
+
+with open('data/skills.json') as skill_file:
+    skills = json.loads(skill_file.read())
+
+skill_list = []
+for skill in skills:
+    name = skill['name']
+    proficiency = skill['proficiency']
+    logo = skill['logo']
+database_model.db.session.add_all(skill_list)    
+database_model.db.session.commit()
